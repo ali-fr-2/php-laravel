730b9a10a97bb5f72171a6e41cdcf30ea842dda5
ali-fr-2 <alifarjamy2@gmail.com>
2026-06-03 23:12:27 +0330

    22-function-array-part1

--- First.php
+++ First.php
@@ -10,19 +10,25 @@
 
     <?php
 
-    // $cars=array(
-    //     array("benz","cyan"),
-    //     array("porche","green"),
-    //     array("volvo","black"),
-    // );
-    // var_dump($cars[0][1]);
+    // $name=array("ali","reza","ahmad","sara");
+    // array_pop($name);
+    // var_dump($name);
 
-    $myFamily=array(
-        array("name"=>'ali','phone'=>914,'age'=>22),
-        array("name"=>'reza','phone'=>914,'age'=>21),
-        array("name"=>'ahmad','phone'=>914,'age'=>23),
-    );
-    var_dump($myFamily[2]["name"]);
+    // array_push($name,"mmd","arash");
+    // var_dump($name);
+
+    // $color1 = array("red", "blue");
+    // $color2 = array("green", "black");
+    // var_dump(array_replace($color1, $color2));
+
+    // $cars=array("benz","bmw","lexus");
+    // var_dump(array_reverse($cars));
+
+    $cars=array("benz","bmw","lexus","tesla");
+    var_dump(array_slice($cars,1,1));
+    var_dump(array_slice($cars,1));
+
+
     ?>
 
 </body>
