afa362bf0706802f342ac3827ec5b4ac731afb3f
ali-fr-2 <alifarjamy2@gmail.com>
2026-06-04 00:22:34 +0330

    25-elseif

--- First.php
+++ First.php
@@ -10,15 +10,45 @@
 
     <?php
 
-    $age=18;
+    // $number=21;
 
-    if ($age>=18) {
-        echo "you should go";
-    }else{
-        echo "you shouldnt go";
-    };
+    // echo ($number%2==0?"even":"odd"); 
+
+    // if ($number%2==0) {
+    //     echo "even";
+    // }
+    // else{
+    //     echo "odd";
+    // };
+
+    // $age=16;
+    // $gender="female";
+
+    // if ($gender=="male") {
+    //     if ($age>=18) {
+    //         echo "you should go";
+    //     }
+    //     else{
+    //         echo "you have time";
+    //     };
+
+    // }
+    // else{
+    //     echo "you are a female";
+    // };
 
 
+        $number=5;
+
+        if ($number>5) {
+            echo "its more than 5";
+        }
+        elseif ($number<5){
+            echo "its less than 5";
+        }
+        else{
+            echo "its equal";
+        }
     ?>
 
 </body>
