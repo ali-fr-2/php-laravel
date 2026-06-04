46c899981a6cf721c8861ae4028670b3b34dce91
ali-fr-2 <alifarjamy2@gmail.com>
2026-06-04 13:08:20 +0330

    27-operators-and

--- First.php
+++ First.php
@@ -10,16 +10,47 @@
 
     <?php
 
-    $num1=6;
-    $num2=8;
+    // $city="";
+    // $price=100;
+    // $person=10;
 
-    if ($num1+$num2==14||$num1*$num2==40) {
+    // if ($city=="yasuj"||"esfahan") {
+    //     if ($person==1) {
+    //         $price=$person*$price;
+    //         $totalPrice=0.99*$price;
+    //         echo $totalPrice;
+    //     }
+    //     if ($person==2) {
+    //         $price=$person*$price;
+    //         $totalPrice=0.98*$price;
+    //         echo $totalPrice;
+    //     }
+    //     if ($person==3) {
+    //         $price=$person*$price;
+    //         $totalPrice=0.97*$price;
+    //         echo $totalPrice;
+    //     }
+    //     if ($person==4) {
+    //         $price=$person*$price;
+    //         $totalPrice=0.96*$price;
+    //         echo $totalPrice;
+    //     }
+    //     if ($person>=5) {
+    //         $price=$person*$price;
+    //         $totalPrice=0.95*$price;
+    //         echo $totalPrice;
+    //     }
+    // }
+
+    $x = 5;
+    $y = 10;
+
+    if ($x + $y == 16 && $x * $y == 50) {
         echo "true";
     }
     else{
         echo "false";
     }
-
 
     ?>
 
