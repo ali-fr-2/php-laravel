c35f8ea40043f7ae9b71d0c011ab0a452f6b4ef0
ali-fr-2 <alifarjamy2@gmail.com>
2026-06-07 17:47:10 +0330

    38-include

---
+++ newfile.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+    echo "hi ali ";
+    // include "First.php";
+    // include "First1.php";
+    require "First.php";
+    // require "First1.php";
+    // echo "<br>";
+    echo "abcd";
+
+?>
+
+
+</body>
+</html>
