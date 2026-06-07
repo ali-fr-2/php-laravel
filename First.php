b26fa1980c9b8c7aa2e63e5c34c3e433d25d184f
ali-fr-2 <alifarjamy2@gmail.com>
2026-06-07 17:17:58 +0330

    37-secondnumber

--- First.php
+++ First.php
@@ -11,16 +11,52 @@
 
     <?php
 
-    // function factorial($x)
-    // {
-    //     if ($x == 0 || $x == 1) {
-    //         return 1;
-    //     }
 
-    //     return $x * factorial($x - 1);
+
+    // function secondHigh(){
+    //     $number=array(22,52,48,548,854,5896,35655,48524);
+    //     $max=$number[0];
+    //     $secondMax=$number[0];
+
+    //     foreach ($number as  $value) {
+    //         if ($value>$max) {
+    //             $secondMax=$max;
+    //             $max=$value;
+    //         }
+    //         else if($value<$max&&$value>$secondMax){
+    //             $secondMax=$value;
+    //         }
+    //     }
+    //     echo $secondMax;
     // }
 
-    // echo factorial(6);
+    // secondHigh();
+
+    $arr = array(22, 35, 545, 44555);
+    function secondMax(array $arr)
+    {
+        if (empty($arr)) {
+            return;
+        }
+        $max = -1;
+        $secondNumber = -1;
+
+        foreach ($arr as $number) {
+            if ($number > $max) {
+                $secondNumber = $max;
+                $max = $number;
+            }
+            if ($number > $secondNumber && $number < $max) {
+                $secondNumber = $max;
+            }
+        }
+        return $secondNumber;
+    }
+
+    echo secondMax($arr);
+
+
+
 
     ?>
 
